b619b5c6af8c7d0a936ca3c8d98a0d7f2d8a478e
Jim Breslin <jbreslin@gmail.com>
2018-06-30 12:50:17 -0400

    move states to state folder

--- player/Player.gd
+++ player/Player.gd
@@ -32,7 +32,6 @@ func _ready():
 func _physics_process(delta):
 	direction = Vector3(0,0,0)
 	if Input.is_action_pressed("ui_left"):
-		print('left')
 		direction.x -= 1
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
